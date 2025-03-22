833f57a3218fc5eb7349f17ec057ddaf17fbc80c
Erizki0712 <erizki13@gmail.com>
2025-03-23 05:17:41 +0800

    resolve conflict reset-password

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,56 +1,8 @@
-// import { FaUser, FaEnvelope, FaLock, FaEye } from "react-icons/fa6";
-// import React from 'react';
-
-// const ResetPassword = () => {
-//   return (
-//     <section className='login min-h-screen flex justify-center items-center relative bg-gradient-to-b from-purple-900 to-white"'>
-//       <div className="relative z-10 bg-transparent sm:bg-white sm:bg-opacity-10 sm:rounded-xl sm:shadow-xl sm:p-4 max-w-2xl w-full sm:backdrop-blur-lg sm:border sm:border-white sm:border-opacity-20">
-//         <div className='flex flex-col md:flex-row justify-center items-center md:gap-8 md:px-10 md:py-12'>
-//           <div className="shrink-1 mb-12 md:p-0 pt-12 pr-12 pl-12 grow-0 basis-auto md:mb-0 md:w-6/12 md:shrink-0 lg:w-6/12 xl:w-6/12 ">
-//             <img
-//                 src="/logo.png"
-//                 className="w-full"
-//                 alt="TravelEase Logo"
-//               />
-//           </div>
-//           <div className='shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-6/12 md:shrink-0 lg:w-6/12 xl:w-6/12'>
-//             <h2 className="text-center text-2xl font-bold text-[#461873] mb-4">Create New Password</h2>
-//             <form>
-//               <div className="mb-4 relative">
-//                 <FaLock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
-//                 <input
-//                   className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#461873]"
-//                   type="password"
-//                   id="password"
-//                   placeholder="New Password"
-//                 />
-//               </div>
-//               <div className="mb-4 relative">
-//                 <FaLock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
-//                 <input
-//                   className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#461873]"
-//                   type="password"
-//                   id="confirm password"
-//                   placeholder="Confirm Password"
-//                 />
-//               </div>
-//               <button className="w-full mt-2 py-2 bg-[#461873] text-white rounded-3xl hover:bg-purple-800 transition duration-300">
-//                   Reset Password
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default ResetPassword
-
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import { resetPassword } from "../redux/actions/authActions";
+import { FaLock } from "react-icons/fa6";
 
 const ResetPassword = () => {
   const [newPassword, setNewPassword] = useState("");
@@ -60,7 +12,7 @@ const ResetPassword = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
-  
+
   const auth = useSelector((state) => state.auth);
   const { loading, error, resetPasswordSuccess } = auth;
 
@@ -92,43 +44,46 @@ const ResetPassword = () => {
   }
 
   return (
-    <section className="login min-h-screen flex justify-center items-center relative bg-gradient-to-b from-purple-900 to-white">
+    <section className="resetPassword min-h-screen flex justify-center items-center relative bg-gradient-to-b from-ungu4 to-ungu9">
       <div className="relative z-10 bg-transparent sm:bg-white sm:bg-opacity-10 sm:rounded-xl sm:shadow-xl sm:p-4 max-w-2xl w-full sm:backdrop-blur-lg sm:border sm:border-white sm:border-opacity-20">
-        <div className="flex flex-col md:flex-row justify-center items-center md:gap-8 md:px-10 md:py-12">
-          <div className="shrink-1 mb-12 md:p-0 pt-12 pr-12 pl-12 grow-0 basis-auto md:mb-0 md:w-6/12 md:shrink-0 lg:w-6/12 xl:w-6/12 ">
-            <img src="/logo.png" className="w-full" alt="Logo" />
+        <div className="flex flex-col md:flex-row justify-center items-center md:gap-10 md:px-10 md:py-12">
+          <div className="shrink-1 mb-12 md:p-0 pt-12 pr-12 pl-12 grow-0 md:w-6/12">
+            <img src="/logo-b.png" className="w-full" alt="TravelEase Logo" />
           </div>
-          <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-6/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-            <h2 className="text-center text-2xl font-bold text-[#461873] mb-4">Reset Password</h2>
+          <div className="shrink-1 mb-12 md:w-6/12">
+            <h2 className="text-center text-2xl font-bold text-ungu1 mb-4">
+              Create New Password
+            </h2>
             <form onSubmit={handleSubmit}>
               <div className="mb-4 relative">
+                <FaLock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
                 <input
                   type="password"
                   id="new-password"
                   placeholder="New Password"
                   value={newPassword}
                   onChange={(e) => setNewPassword(e.target.value)}
-                  className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#461873]"
+                  className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-ungu1"
                 />
               </div>
               <div className="mb-4 relative">
+                <FaLock className="absolute left-4 top-1/2 transform -translate-y-1/2 text-purple-500" />
                 <input
                   type="password"
                   id="confirmation-password"
                   placeholder="Confirm Password"
                   value={confirmationPassword}
                   onChange={(e) => setConfirmationPassword(e.target.value)}
-                  className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-[#461873]"
+                  className="w-full p-2 pl-10 pr-4 py-2 rounded-lg bg-white bg-opacity-20 text-black placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-ungu1"
                 />
               </div>
               <button
                 type="submit"
-                className="w-full mt-2 py-2 bg-[#461873] text-white rounded-3xl hover:bg-purple-800 transition duration-300"
+                className="w-full mt-2 py-2 bg-ungu5 text-white rounded-3xl hover:bg-ungu6 transition duration-300"
               >
                 {loading ? "Resetting..." : "Reset Password"}
               </button>
             </form>
-
             {error && <p className="text-red-500 text-center mt-4">{error}</p>}
           </div>
         </div>
